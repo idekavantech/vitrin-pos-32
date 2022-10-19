39532e7c12971e58ee324bc6b9b5d29d84c40c00
Mohammad Norouzi <mnorouzidev@gmail.com>
2022-10-19 10:27:15 +0330

    Change back createWindowsInstaller function

--- createinstaller.js
+++ createinstaller.js
@@ -2,50 +2,28 @@ const createWindowsInstaller = require('electron-winstaller').createWindowsInsta
 const path = require('path')
 const VERSION = process.env.npm_package_version;
 
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
 
-
-try {
+function getInstallerConfig() {
   console.log('creating windows installer')
   const rootPath = path.join('./')
   const outPath = path.join(rootPath, 'build')
-  await createWindowsInstaller({
+
+  return Promise.resolve({
     appDirectory: path.join(outPath, 'vitrin-pos-win32-x64/'),
+    // appDirectory: path.join(outPath, 'vitrin-pos-win32-ia32/'),
+    authors: 'Idekavan Inc',
     noMsi: true,
     outputDirectory: path.join(outPath, 'installable'),
-    authors: 'Idekavan Inc.',
     exe: 'vitrin-pos.exe',
     setupExe: `VitrinPOS-${VERSION}-64bit.exe`,
     setupIcon: path.join(rootPath, 'assets', 'icon.ico'),
     skipUpdateIcon: true,
     loadingGif: path.join(rootPath, 'assets', 'loading.gif'),
   })
-} catch (e) {
-  console.log(`No dice: ${e.message}`)
 }
-//
-//
-// getInstallerConfig()
-//   .then(createWindowsInstaller)
-//   .catch((error) => {
-//     console.error(error.message || error)
-//     process.exit(1)
-//   })
-//
-// function getInstallerConfig() {
-//   console.log('creating windows installer')
-//   const rootPath = path.join('./')
-//   const outPath = path.join(rootPath, 'build')
-//
-//   return Promise.resolve({
-//     appDirectory: path.join(outPath, 'vitrin-pos-win32-x64/'),
-//     // appDirectory: path.join(outPath, 'vitrin-pos-win32-ia32/'),
-//     authors: 'komeilmehranfar',
-//     noMsi: true,
-//     outputDirectory: path.join(outPath, 'installable'),
-//     exe: 'vitrin-pos.exe',
-//     setupExe: `VitrinPOS-${VERSION}-64bit.exe`,
-//     setupIcon: path.join(rootPath, 'assets', 'icon.ico'),
-//     skipUpdateIcon: true,
-//     loadingGif: path.join(rootPath, 'assets', 'loading.gif'),
-//   })
-// }
