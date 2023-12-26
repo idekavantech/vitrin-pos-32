ae63143ba4273ebc9b3c9c71c92f67932516417d
Mohammad Norouzi <mnorouzidev@gmail.com>
2023-12-26 13:05:46 +0330

    fix: update author of create installer

--- createinstaller.js
+++ createinstaller.js
@@ -1,29 +1,30 @@
-const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
-const path = require('path')
+const createWindowsInstaller = require("electron-winstaller")
+  .createWindowsInstaller;
+const path = require("path");
 const VERSION = process.env.npm_package_version;
 
 getInstallerConfig()
   .then(createWindowsInstaller)
   .catch((error) => {
-    console.error(error.message || error)
-    process.exit(1)
-  })
+    console.error(error.message || error);
+    process.exit(1);
+  });
 
 function getInstallerConfig() {
-  console.log('creating windows installer')
-  const rootPath = path.join('./')
-  const outPath = path.join(rootPath, 'build')
+  console.log("creating windows installer");
+  const rootPath = path.join("./");
+  const outPath = path.join(rootPath, "build");
 
   return Promise.resolve({
     //appDirectory: path.join(outPath, 'vitrin-pos-win32-x64/'),
-    appDirectory: path.join(outPath, 'vitrin-pos-win32-ia32/'),
-    authors: 'komeilmehranfar',
+    appDirectory: path.join(outPath, "vitrin-pos-win32-ia32/"),
+    authors: "idekavan",
     noMsi: true,
-    outputDirectory: path.join(outPath, 'installable'),
-    exe: 'vitrin-pos.exe',
+    outputDirectory: path.join(outPath, "installable"),
+    exe: "vitrin-pos.exe",
     setupExe: `VitrinPOS-${VERSION}-32bit.exe`,
-    setupIcon: path.join(rootPath, 'assets', 'icon.ico'),
+    setupIcon: path.join(rootPath, "assets", "icon.ico"),
     skipUpdateIcon: true,
-    loadingGif: path.join(rootPath, 'assets', 'loading.gif'),
-  })
+    loadingGif: path.join(rootPath, "assets", "loading.gif"),
+  });
 }
