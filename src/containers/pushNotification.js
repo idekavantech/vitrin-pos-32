7aa4d46f34ed53262b92516d82e7c9377ab4605f
Mohammad Norouzi <mnorouzidev@gmail.com>
2024-08-03 15:20:39 +0330

    feat: update socket url

--- src/containers/pushNotification.js
+++ src/containers/pushNotification.js
@@ -1,6 +1,5 @@
 import { Manager } from "socket.io-client";
 
-
 export default function initPushNotification(
   showSnackBar,
   history,
@@ -10,23 +9,23 @@ export default function initPushNotification(
   const { ipcRenderer } = require("electron");
   const { getCurrentWindow } = require("@electron/remote");
 
-  const manager = new Manager("480da50e-020d-401c-a5ee-9341cbecc341.hsvc.ir:30157", {
+  const manager = new Manager("https://socket.vitrin.me", {
     reconnectionDelayMax: 10000,
   });
 
   const socket = manager.socket("/", {});
 
   socket.on("msg", (item) => {
-    console.log(item,"item")
+    console.log(item, "item");
     receiveOrder(item);
   });
 
   manager.on("open", () => {
-    console.log("socket opened")
+    console.log("socket opened");
     socket.emit("register", businessSlugs);
   });
 
-  ipcRenderer.on("redirectOrder", (event, {id}) => {
+  ipcRenderer.on("redirectOrder", (event, { id }) => {
     history.push(`/orders/${id}`);
     getCurrentWindow().show();
   });
